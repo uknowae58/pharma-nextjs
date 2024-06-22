603a37ad378e39d4dbbeb0210105dfa31adcedba
uknowae <angemmanuel58@gmail.com>
2024-06-22 00:18:57 +0000

    fisrt commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Component } from "@/components/component";
+import { Dashboard } from "@/components/dashboard";
+import Image from "next/image";
+
+
+export default function Home() {
+    return <Dashboard/>;
+  
+}
